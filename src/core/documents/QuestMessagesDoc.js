2be0b4922cf79266699e91facfdc073985efccdb
R13 <rbentos@yahoo.com>
2023-06-29 22:14:22 -0300

    QuestMessagesDoc.getDescription()

--- src/core/documents/QuestMessagesDoc.js
+++ src/core/documents/QuestMessagesDoc.js
@@ -3,6 +3,14 @@ class QuestMessagesDoc {
 
   static type = 'questMessages'
 
+  static getDescription(store, timestamp) {
+    const doc = store.getDocument(QuestMessagesDoc.type, timestamp)
+    if (!doc) return '?'
+    const date = new Date(parseInt(timestamp)).toDateString()
+    const messages = doc.messages ? JSON.parse(doc.messages) : []
+    return `${date} ${doc.chamber}+${doc.player} (${messages.length} msgs) ${messages[0]?.content?.slice(0,0) ?? ''}`
+  }
+
   static updateMessages(store, timestamp, realmCoord, chamberSlug, player, messages) {
     const _messages = messages.length > 4 ? messages.slice(4) : null
     const data = {
