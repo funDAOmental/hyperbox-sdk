57f8abf9243500bf65b578be2236ca9857dc0b69
R13 <rbentos@yahoo.com>
2023-06-30 15:52:24 -0300

    QuestMessagesDoc.getEncounters()

--- src/core/documents/QuestMessagesDoc.js
+++ src/core/documents/QuestMessagesDoc.js
@@ -4,7 +4,7 @@ class QuestMessagesDoc {
   static type = 'questMessages'
 
   static getDescription(store, timestamp) {
-    const doc = store.getDocument(QuestMessagesDoc.type, timestamp)
+    const doc = store.getDocument(QuestMessagesDoc.type, timestamp.toString())
     if (!doc) return '?'
     const date = new Date(parseInt(timestamp)).toDateString()
     const messages = doc.messages ? JSON.parse(doc.messages) : []
@@ -21,6 +21,20 @@ class QuestMessagesDoc {
     }
     store.upsertDocument(QuestMessagesDoc.type, timestamp.toString(), data)
   }
+
+  static getEncounters(store, chamberSlug, realmCoord = null) {
+    const result = {}
+    const ids = store.getIds(QuestMessagesDoc.type)
+    for (const id of ids) {
+      const doc = store.getDocument(QuestMessagesDoc.type, id)
+      if (doc && doc.chamber == chamberSlug && (doc.realm == parseInt(realmCoord) || realmCoord === null)) {
+        result[id] = {...doc}
+      }
+    }
+    console.log(`ENCOUNTERS:`, chamberSlug, realmCoord, result)
+    return result
+  }
+
 }
 
 export {
